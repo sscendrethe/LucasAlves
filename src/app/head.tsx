5a9c960d78ff4d15eb94b25e03098ed002266ca2
sscendrethe <stephanie_cendrethe@hotmail.com>
2025-08-31 19:14:20 -0300

    Add logo icon

---
+++ src/app/head.tsx
@@ -0,0 +1,19 @@
+export default function Head() {
+  return (
+    <>
+      <title>Agencia EuLukasAlves</title>
+      <meta name="description" content="Venha conhecer os nossos serviços!" />
+
+      {/* Open Graph */}
+      <meta property="og:title" content="Agencia EuLukasAlves" />
+      <meta property="og:description" content="Venha conhecer os nossos serviços!" />
+      <meta property="og:image" content="/images/logo/logo.png" />
+      <meta property="og:url" content="https://eulukasalves.vercel.app/" />
+      <meta property="og:type" content="website" />
+
+      {/* Favicon */}
+      <link rel="icon" href="/images/logo/logo.png" type="image/png" />
+    </>
+  )
+}
+
