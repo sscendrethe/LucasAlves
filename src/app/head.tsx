8564f17b5f53fbdaf9a184dd18ec3ec26f142622
sscendrethe <stephanie_cendrethe@hotmail.com>
2025-09-01 17:50:20 -0300

    Ajuste root IV

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -5,7 +5,7 @@ export default function Head() {
       <meta name="description" content="Venha conhecer os nossos serviços!" />
 
       {/* Open Graph */}
-      <meta property="og:title" content="Agencia EuLukasAlves" />
+      <meta property="og:title" content="Agência EuLukasAlves" />
       <meta property="og:description" content="Venha conhecer os nossos serviços!" />
       <meta property="og:image" content="/images/logo/logo.png" />
       <meta property="og:url" content="https://eulukasalves.vercel.app/" />
